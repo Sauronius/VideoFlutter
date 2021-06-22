4f2ce758899eef20f0b7201e157b8a47c5dce863
Sauronius <cisekdmn@gmail.com>
2021-06-22 09:21:31 +0200

    Add basic controls attached to the video

--- lib/VideoRoute.dart
+++ lib/VideoRoute.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:dc_video_player/video_player_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
 
@@ -42,7 +43,6 @@ class _VideoRouteState extends State<VideoRoute> {
     }
   }
 
-  Future<void> _initializeVideoPlayerFeature;
   File args;
 
   @override
@@ -62,77 +62,64 @@ class _VideoRouteState extends State<VideoRoute> {
         // the App.build method, and use it to set our appbar title.
         title: Text("$args"),
       ),
-      body: FutureBuilder(
-        future: _initializeVideoPlayerFeature,
-        builder: (context, snapshot) {
-          return Column(children: <Widget>[
-            AspectRatio(
-              aspectRatio: playerController.value.aspectRatio,
-              child: VideoPlayer(playerController),
-            ),
-            Row(
-              children: [
-                Slider(
-                    value: playerController.value.volume,
-                    max: 1,
-                    min: 0,
-                    onChanged: (value) {
-                      playerController.setVolume(value);
-                    }),
-                Icon(playerController.value.volume != 0
-                    ? Icons.volume_up
-                    : Icons.volume_off)
-              ],
-            ),
-            Row(
-              children: [
-                TextButton(
-                  child: Text("2x"),
-                  onPressed: () {
-                    playerController.setPlaybackSpeed(2.0);
-                  },
-                ),
-                TextButton(
-                  child: Text("1.5x"),
-                  onPressed: () {
-                    playerController.setPlaybackSpeed(1.5);
-                  },
-                ),
-                TextButton(
-                  child: Text("1.25x"),
-                  onPressed: () {
-                    playerController.setPlaybackSpeed(1.25);
-                  },
-                ),
-                TextButton(
-                  child: Text("1x"),
-                  onPressed: () {
-                    playerController.setPlaybackSpeed(1.0);
-                  },
-                ),
-                TextButton(
-                  child: Text("0.5x"),
-                  onPressed: () {
-                    playerController.setPlaybackSpeed(0.5);
-                  },
-                ),
-                TextButton(
-                  child: Text("0.25x"),
-                  onPressed: () {
-                    playerController.setPlaybackSpeed(0.25);
-                  },
-                ),
-              ],
-            )
-          ]);
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          togglePlay();
-        },
-        child: Icon(
-            playerController.value.isPlaying ? Icons.pause : Icons.play_arrow),
+      body: Column(
+        children: [
+          VideoPlayerWidget(controller: playerController),
+          Row(
+            children: [
+              Slider(
+                  value: playerController.value.volume,
+                  max: 1,
+                  min: 0,
+                  onChanged: (value) {
+                    playerController.setVolume(value);
+                  }),
+              Icon(playerController.value.volume != 0
+                  ? Icons.volume_up
+                  : Icons.volume_off)
+            ],
+          ),
+          Row(
+            children: [
+              TextButton(
+                child: Text("2x"),
+                onPressed: () {
+                  playerController.setPlaybackSpeed(2.0);
+                },
+              ),
+              TextButton(
+                child: Text("1.5x"),
+                onPressed: () {
+                  playerController.setPlaybackSpeed(1.5);
+                },
+              ),
+              TextButton(
+                child: Text("1.25x"),
+                onPressed: () {
+                  playerController.setPlaybackSpeed(1.25);
+                },
+              ),
+              TextButton(
+                child: Text("1x"),
+                onPressed: () {
+                  playerController.setPlaybackSpeed(1.0);
+                },
+              ),
+              TextButton(
+                child: Text("0.5x"),
+                onPressed: () {
+                  playerController.setPlaybackSpeed(0.5);
+                },
+              ),
+              TextButton(
+                child: Text("0.25x"),
+                onPressed: () {
+                  playerController.setPlaybackSpeed(0.25);
+                },
+              ),
+            ],
+          )
+        ],
       ),
     );
   }
